48e93aa750a0f38b078d2829c951403868a4eac0
Eric Aniag <ericaniag@gmail.com>
2019-07-30 02:02:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function createRock(x) {
 
 function endGame() {
   clearInterval(gameInterval)
-  const rock = document.div.rock
+  const rock = 
   for (var i = 1; i < ROCKS.length; i++) {
     rock.remove()
   }
